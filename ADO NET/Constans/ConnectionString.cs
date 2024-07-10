00c2aab29d7b736af36d1b709b3a3c4150c7c8f5
unknown <alivj@code.edu.az>
2024-07-10 12:31:25 +0400

    added

---
+++ ADO NET/Constans/ConnectionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO_NET.Constans
+{
+    public static class ConnectionString
+    {
+        public static string Default = "Server=DESKTOP-37AB6R7\\SQLEXPRESS;Database=MyDatabase;Trusted_Connection=True;";
+    }
+}
---
+++ ADO NET/Constans/Operations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO_NET.Constans
+{
+    public enum Operations
+    {
+        Exit,
+        AllCountries,
+        AddCountry,
+        UpdateCountry,
+        DeleteCountry,
+        DetailsofCountry,
+        AllCities,
+        AllCitiesofCountry,
+        AddCity,
+        UpdateCity,
+        DeleteCity,
+        DetailsofCity
+    }
+   
+}
